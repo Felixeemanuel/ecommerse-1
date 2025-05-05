f816d264ecb606365e7cf17a830c498fcc2aff87
Felix Edström <felix.edstrom1@gmail.com>
2025-05-05 21:26:05 +0200

    pages folder setup in progress

--- ecommerse-1/src/main.jsx
+++ ecommerse-1/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <div>404 - Page not found</div>,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  }
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
